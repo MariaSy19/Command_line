cb2adf9976b10f0ee0fc3ddb8ee33e3f9f4516b7
MariaSy19 <101586605+MariaSy19@users.noreply.github.com>
2024-02-18 21:21:18 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Terminal terminal = new Terminal();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true)
+        {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+
+            if (input.equals("exit") )
+            {
+                break;
+            }
+
+            terminal.histComm.add(input);
+
+            String[] inputParts = input.split(" ");
+            String command = inputParts[0];
+            String[] arguments = new String[inputParts.length - 1];
+            System.arraycopy(inputParts, 1, arguments, 0, arguments.length);
+
+            terminal.chooseCommandAction(command, input);
+        }
+        scanner.close();
     }
 }
---
+++ src/Parser.java
@@ -0,0 +1,53 @@
+public class Parser
+{
+
+    String commandName;
+    String[] args;
+
+    // This method will divide the input into commandName and args
+    // where "input" is the string command entered by the user
+    public boolean parse(String input)
+    {
+        //split to get individual string
+        String[] str = input.split(" ");
+        //check if there are no strings
+        if (str.length == 0)
+        {
+            System.out.println("Invalid command! ");
+            return false; //parsing failure
+        }
+        commandName = str[0]; //first string assumed command name
+        args = new String[str.length - 1]; // The rest of the strings (if any) are considered as arguments
+        System.arraycopy(str, 1, args, 0, str.length - 1);// Copy the arguments from string into the args array
+        if(( commandName.equals("pwd")) && args.length != 0)
+        {
+            System.out.println("ERROR: " + commandName + " not take any argumnent!");
+            return false;
+        }
+
+        if (( commandName.equals("cp")||( commandName.equals("cp-r"))) && args.length != 2) {
+            System.out.println("ERROR: " + commandName + " take two arguments!");
+            return false;
+        }
+        if(( commandName.equals("rmdir") ||commandName.equals("echo") || commandName.equals("mkdir") ||commandName.equals("cat") ) && args.length == 0 )
+        {
+            System.out.println("ERROR: " + commandName + " take at least 1 argument!");
+            return false;
+        }
+        if(( commandName.equals("touch")
+                ||commandName.equals("rm") ) && args.length != 1 )
+        {
+            System.out.println("ERROR: " + commandName + " take one argument!");
+            return false;
+        }
+        return true;//parsing successful
+    }
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
+}
